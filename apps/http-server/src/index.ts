70c6caa8a730e1b8006158d4470015b1e6cfd171
Parashar Deb <parashardeb11@gmail.com>
2025-07-04 23:58:48 +0530

    db resolved

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,21 +1,41 @@
 import  express from "express";
 import jwt from "jsonwebtoken"
 import { Createroomschema } from "@repo/common/types";
-import { Userschema } from "@repo/common/types";
+
+import { Userschema,Singin} from "@repo/common/types";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 const app=express();
 app.post("/signup",(req,res)=>{
     const data=Userschema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message:"Invalid input"
+        })
+        return
+    }
 })
 app.post("/signin",(req,res)=>{
-    const username=req.body.username;
-    const password=req.body.password;
+    const data=Singin.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message:"Invalid Input"
+        })
+        return
+    }
+    const userId=1
     const token =jwt.sign({
-        username
+        userId 
     },JWT_SECRET)
 })
 app.get("/room",middleware,(req,res)=>{
+    const data=Createroomschema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message:"Invalid Input"
+        })
+        return
+    }
     res.send("hi there")
 })
 app.listen(3001)
