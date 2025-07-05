b749cec88d3fe626639c883c5774a2c302a5b08e
Parashar Deb <parashardeb11@gmail.com>
2025-07-05 21:47:26 +0530

    ws-server-authentication

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -3,15 +3,24 @@ import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss=new WebSocketServer({port:8080})
+function userAuthethtication(token:string): string | null {
+    const decoded=jwt.verify(token,JWT_SECRET)
+    if(typeof(decoded)=="string"){
+        return null
+    }
+    if(!decoded || !(decoded as JwtPayload).userId){
+        return null;
+    }
+    return decoded.userId
+}
 wss.on("connection",(ws,request)=>{
     const url=request.url
     if(!url)
         return
     const queryParams=new URLSearchParams(url.split("?")[1])
     const token=queryParams.get("token")||" "
-    const decoded=jwt.verify(token,JWT_SECRET)
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
+    const user =userAuthethtication(token); 
+    if(!user){
+        ws.close()
     }
 })
