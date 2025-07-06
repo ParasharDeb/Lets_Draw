73e13000cf2ded42df154e4427c0e6dd59ba7383
Parashar Deb <parashardeb11@gmail.com>
2025-07-06 22:51:42 +0530

    uhhh...

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,9 +6,34 @@ export default function Home() {
   const [RoomId,setRoomId]=useState("")
   const router=useRouter()
   return (
-    <div className={styles.page}>
-      <input type="text" placeholder="Enter RoomId" value={RoomId} onChange={(e)=>setRoomId(e.target.value)}/>
-      <button onClick={()=>{
+    <div style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      width:"100vw",
+      height:"100vh",
+
+    }}>
+      <input  style={{
+        padding:8,
+        borderRadius:4,
+        border:"1px solid #ccc",
+        fontSize:16,
+        width:300,
+        outline:"none"
+      }}
+      type="text" placeholder="Enter RoomId" value={RoomId} onChange={(e)=>setRoomId(e.target.value)}/>
+      <button style={{
+        backgroundColor:"blue",
+        padding:"8px 16px",
+        color:"white",
+        borderRadius:4,
+        marginLeft:10,
+        cursor:"pointer",
+        border:"none",
+        fontSize:16,
+        fontWeight:"bold"
+      }} onClick={()=>{
         router.push(`/room/${RoomId}`)
       }}>Join Room</button>
     </div>
