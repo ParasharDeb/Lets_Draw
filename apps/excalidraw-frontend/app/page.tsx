dff9f250422ebc969229a7067a879fb351508c8f
Parashar Deb <parashardeb11@gmail.com>
2025-07-14 16:02:00 +0530

    frontend restart

--- apps/excalidraw-frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      Excalidraw frontend
-    </div>
-  );
-}
--- apps/excalidraw-frontend/app/signin/page.tsx
+++
@@ -1,8 +0,0 @@
-import Authentication from "../components/authen"
-export default function Signin(){
-    return(
-        <div>
-            <Authentication issignedIn={true}/>
-        </div>
-    )
-}
