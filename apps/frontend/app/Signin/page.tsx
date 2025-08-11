5303f34ad6b4024a2a8e9e09714927222ea3988f
ParasharDeb <parashardeb11@gmail.com>
2025-08-11 19:56:31 +0530

    added signin logic

--- apps/frontend/app/Signin/page.tsx
+++ apps/frontend/app/Signin/page.tsx
@@ -1,9 +1,52 @@
+'use client';
 import Button from "@/ui/button";
+import InputBox from "@/ui/input";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+export default function Signin() {
 
-export default function Signup() {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const Router = useRouter();
+    function handleSignup() {
+        try{
+            axios.post("/api/signin",{
+                password: password,
+                email: email,
+            }).then((response) => {
+                console.log(response.data);
+                Router.push("/dashboard");
+                if (response.status === 200) {
+                    alert("Signin successful!");
+                } else {
+                    alert("Signin failed: " + response.data.message);
+                }
+            }
+            ).catch((error) => {
+                console.error("Error during signin:", error);
+                alert("Signin failed. Please try again.");
+            });
+        }
+        catch (error) {
+            console.error("Signin failed:", error);
+            alert("Signup failed. Please try again.Database is down");
+        }
+    }
     return(
-        <Button onClick={(e) => alert("Button clicked!")} variant="primary" size="lg">
-            Sign Up
-        </Button>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
+                <h1 className="text-2xl font-bold mb-6 text-center text-black">Sign In</h1>
+                
+                <InputBox inputType="email" placeholder="Email" onChangehandler={(e) => setEmail(e.target.value)} />
+                <InputBox inputType="password" placeholder="Password" onChangehandler={(e) => setPassword(e.target.value)} />
+                <Button onClick={(e) => alert("Button clicked!")} variant="primary" size="lg">
+                    Sign In
+                </Button>
+                <p className="mt-4 text-sm text-gray-600 text-center">
+                    Dont have an account? <a href="/signup" className="text-blue-500 hover:underline">Sign Up</a>
+                </p>
+            </div>
+        </div>
     )
 }
--- apps/frontend/app/Signup/page.tsx
+++ apps/frontend/app/Signup/page.tsx
@@ -3,12 +3,12 @@ import Button from "@/ui/button";
 import InputBox from "@/ui/input";
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-    const Router = useRouter();
+    const router = useRouter();
     function handleSignup() {
         try{
             axios.post("/api/signup",{
@@ -17,7 +17,7 @@ export default function Signup() {
                 email: email,
             }).then((response) => {
                 console.log(response.data);
-                Router.push("/signin");
+                router.push("/signin");
                 if (response.status === 200) {
                     alert("Signup successful!");
                 } else {
@@ -36,12 +36,12 @@ export default function Signup() {
     }
     return(
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
                 <h1 className="text-2xl font-bold mb-6 text-center text-black">Sign Up</h1>
                 <InputBox inputType="text" placeholder="Username" onChangehandler={(e) => setUsername(e.target.value)} />
                 <InputBox inputType="email" placeholder="Email" onChangehandler={(e) => setEmail(e.target.value)} />
                 <InputBox inputType="password" placeholder="Password" onChangehandler={(e) => setPassword(e.target.value)} />
-                <Button onClick={(e) => alert("Button clicked!")} variant="primary" size="lg">
+                <Button onClick={handleSignup} variant="primary" size="lg">
                     Sign Up
                 </Button>
                 <p className="mt-4 text-sm text-gray-600 text-center">
